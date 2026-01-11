837d057bd655926db4633d732f5b441a71785719
Kinorw <otherlittle66@gmail.com>
2026-01-11 18:51:31 +0800

    cache lab 64 1700

--- cachelab/trans.c
+++ cachelab/trans.c
@@ -20,9 +20,81 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  *     be graded. 
  */
 char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
+    int blockSize = 8, tmp;
+    for(int i = 0; i < N; i += blockSize){
+        for(int j = 0 ; j < M ; j += blockSize){
+            if (M ==32){
+                for (int k = i ; k < i + blockSize && k < N ; ++ k){
+                    for(int l = j ; l < j + blockSize && l < M ; ++ l){
+                        if (k != l){
+                            B[l][k] = A[k][l]; 
+                        }                   
+                        else tmp = A[k][l];
+                    }
+                    if(i == j) B[k][k]=tmp;
+                }
+            }
+            else if(M==64){
+                int miniBlock = 4;
+                int t1,t2,t3,t4;
+                for(int k = i; k < i + miniBlock ; ++ k){
+                    for(int l = j ; l < j + miniBlock ; ++ l){
+                        if( k != l){
+                            B[l][k] = A[k][l];
+                        }
+                        else tmp = A[k][l];
+                    }
+                    if(i==j) B[k][k]=tmp;
+                }
+                for(int k = i; k < i + miniBlock ; ++ k){
+                    t1 = A[k][j+miniBlock];
+                    t2 = A[k][j+miniBlock+1];
+                    t3 = A[k][j+miniBlock+2];
+                    t4 = A[k][j+miniBlock+3];
+                    B[j+miniBlock][k] = t1;
+                    B[j+miniBlock+1][k] = t2;
+                    B[j+miniBlock+2][k] = t3;
+                    B[j+miniBlock+3][k] = t4;
+                }
+                for(int k = i + miniBlock; k < i + 2*miniBlock ; ++ k){
+                    t1 = A[k][j];
+                    t2 = A[k][j+1];
+                    t3 = A[k][j+2];
+                    t4 = A[k][j+3];
+                    B[j][k] = t1;
+                    B[j+1][k] = t2;
+                    B[j+2][k] = t3;
+                    B[j+3][k] = t4;
+                }
+                for(int k = i + miniBlock; k < i + 2*miniBlock ; ++ k){
+                    for(int l = j + miniBlock; l < j + 2*miniBlock ; ++ l){
+                        if( k != l){
+                            B[l][k] = A[k][l];
+                        }
+                        else tmp = A[k][l];
+                    }
+                    if(i==j) B[k][k]=tmp;
+                }
+            }
+        }
+    }
 }
+/*
+当循环进行到 l == k 时：
+
+读 A[k][k]：读入 tmp。不触碰 B。Row_A_k 保留在缓存中。
+进入下一次迭代 l = k + 1：
+读 A[k][k+1]：命中！(Saved A Miss)。
+循环结束：
+写 B[k][k] = tmp：此时这一行的 A 已经读完了。加载 Row_B_k，驱逐 Row_A_k。
+关键点：Row_B_k 现在留在了缓存中。
+在下一行循环（Row k+1）时：
+当需要写入 B[k][k+1] 时，Row_B_k 还在缓存中。
+命中！ (Saved B reload)。
+总结：优化后，对于每一行k，我们要么避免了 A 的重载，要么保留了 B 供下次使用。
+(与使用8个寄存器效果相仿)
+*/
 
 /* 
  * You can define additional transpose functions below. We've defined
@@ -59,7 +131,7 @@ void registerFunctions()
     registerTransFunction(transpose_submit, transpose_submit_desc); 
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
+    // registerTransFunction(trans, trans_desc); 
 
 }
 
